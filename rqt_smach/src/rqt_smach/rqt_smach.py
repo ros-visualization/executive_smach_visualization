83817a4aeec8e3309796d51087ff2ce3e2f6f560
Jonathan Bohren <jbo@jhu.edu>
2012-12-13 22:09:06 -0500

    Adding initial (empty) rqt_smach plugin

---
+++ rqt_smach/src/rqt_smach/rqt_smach.py
@@ -0,0 +1,57 @@
+import os
+import roslib
+roslib.load_manifest('rqt_smach')
+import rospy
+
+from qt_gui.plugin import Plugin
+from python_qt_binding import loadUi
+from python_qt_binding.QtGui import QWidget
+
+class SMACHVisualization(Plugin):
+
+    def __init__(self, context):
+        super(SMACHVisualization, self).__init__(context)
+        # Give QObjects reasonable names
+        self.setObjectName('SMACHVisualization')
+
+        # Process standalone plugin command-line arguments
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        # Add argument(s) to the parser.
+        parser.add_argument("-q", "--quiet", action="store_true",
+                      dest="quiet",
+                      help="Put plugin in silent mode")
+        args, unknowns = parser.parse_known_args(context.argv())
+        if not args.quiet:
+            print 'arguments: ', args
+            print 'unknowns: ', unknowns
+
+        # Create QWidget
+        self._widget = QWidget()
+        # Get path to UI file which is a sibling of this file
+        # in this example the .ui and .py file are in the same folder
+        ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'SMACHVisualization.ui')
+        # Extend the widget with all attributes and children from UI file
+        loadUi(ui_file, self._widget)
+        # Give QObjects reasonable names
+        self._widget.setObjectName('SMACHVisualizationUi')
+        # Add widget to the user interface
+        context.add_widget(self._widget)
+
+    def shutdown_plugin(self):
+        # TODO unregister all publishers here
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
+
+    #def trigger_configuration(self):
+        # Comment in to signal that the plugin has a way to configure it
+        # Usually used to open a configuration dialog
